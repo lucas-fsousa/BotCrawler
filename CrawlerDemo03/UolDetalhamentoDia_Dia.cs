4589e7e3e3c3762133c45722ed7c3b4136801e6d
Lucas de Sousa <Lucasads18@outlook.com>
2021-11-09 15:54:33 -0300

    new att

--- CrawlerDemo03/UolDetalhamentoDia_Dia.cs
+++ CrawlerDemo03/UolDetalhamentoDia_Dia.cs
@@ -13,15 +13,15 @@ namespace CrawlerDemo03 {
 
     public static DetailsNotice ReadByLink(string link) {
       var details = new DetailsNotice();
+      List<HtmlNode> text = null;
       try {
         var document = new HtmlDocument();
         using(var wc = new WebClient()) {
           document.LoadHtml(wc.DownloadString(link));
         }
         // checks nullable
-        var check = $"{document.DocumentNode.SelectNodes("//div[@class='  author  ']")?.FirstOrDefault()}";
         var divBase = document.DocumentNode.SelectNodes("//div[@class='  author  ']")?.FirstOrDefault();
-        if(check.Equals("")) {
+        if($"{document.DocumentNode.SelectNodes("//div[@class='  author  ']")?.FirstOrDefault()}".Equals("")) {
           details.Autor = "UOL";
           details.PublishDate = $"{document.DocumentNode.SelectSingleNode("//time[@class='c-more-options__published-date']")?.InnerText}";
           details.AutorLocation = "SP - São Paulo";
@@ -29,11 +29,27 @@ namespace CrawlerDemo03 {
           details.Autor = $"{divBase.Descendants("p")?.Where(x => x.Attributes["class"].Value.Equals("p-author"))?.FirstOrDefault()?.InnerText}".Trim();
           details.PublishDate = $"{divBase.Descendants("p")?.Where(x => x.Attributes["class"].Value.Equals("p-author time"))?.FirstOrDefault()?.InnerText.ToString()}".Trim();
           details.AutorLocation = $"{divBase.Descendants("p")?.Where(x => x.Attributes["class"].Value.Equals("p-author-local"))?.FirstOrDefault()?.InnerText.ToString()}".Trim();
+
           if(details.Autor.Equals("")) details.Autor = "Autor desconhecido / Colaboração Externa";
-          
-          foreach(var p in document.DocumentNode.SelectNodes("//div[@class='text  ']").FirstOrDefault().Descendants("p").ToList()) {
-            details.PublishNotes += $"{p.InnerText}\n";
-          }
+        }
+
+        string divText = $"{document.DocumentNode.SelectNodes("//div[@class='text  ']")?.FirstOrDefault()?.Descendants("p")?.ToList()}";
+        string divTextSection = $"{ document.DocumentNode.SelectNodes("//div[@class='c-news__body']")?.FirstOrDefault()?.Descendants("p")?.ToList()}";
+        string divAlt = $"{document.DocumentNode.SelectNodes("//div[@id='modal-ready']")?.FirstOrDefault()?.Descendants("p")?.ToList()}";
+        string divTextAlt = $"{document.DocumentNode.SelectNodes("//div[@class='text']")?.FirstOrDefault()?.Descendants("p")?.ToList()}";
+
+        if(!divText.Equals("")) {
+          text = document.DocumentNode.SelectNodes("//div[@class='text  ']").FirstOrDefault().Descendants("p").ToList();
+        }else if(!divTextSection.Equals("")) {
+          text = document.DocumentNode.SelectNodes("//div[@class='c-news__body']").FirstOrDefault().Descendants("p").ToList();
+        }else if(!divAlt.Equals("")) {
+          text = document.DocumentNode.SelectNodes("//div[@id='modal-ready']").FirstOrDefault().Descendants("p").ToList();
+        }else if(!divTextAlt.Equals("")) {
+          text = document.DocumentNode.SelectNodes("//div[@class='text']").FirstOrDefault().Descendants("p").ToList();
+        }
+
+        foreach(var p in text) {
+          details.PublishNotes += $"{p.InnerText}\n";
         }
       } catch(Exception ex) {
         Console.BackgroundColor = ConsoleColor.Red;
