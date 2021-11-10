b146935e8b3d7cf18eda5fcb56a97bcae8119066
Lucas de Sousa <Lucasads18@outlook.com>
2021-11-10 17:03:29 -0300

    new att

--- CrawlerDemo02/UolControl.cs
+++ CrawlerDemo02/UolControl.cs
@@ -11,15 +11,17 @@ using System.Threading.Tasks;
 namespace CrawlerDemo02 {
   public static class UolControl {
     public static async Task ExecuteReader() {
-      var newNotices = await UolHomePage.GoScan();
-      foreach(var notice in newNotices) {
-        if(notice.PublishNotes.Length > 1) {
-          Console.WriteLine($"\t{notice.Title}\n\t\t{notice.Description}\n >> {notice.Link}\n");
-          Console.WriteLine($"  {notice.Autor} - {notice.AutorLocation} - {notice.PublishDate}");
-          Console.WriteLine($"\t{notice.PublishNotes}");
-          Console.WriteLine("====================================================================\n");
-          Thread.Sleep(1000); // espera 10 segundos
-          Console.Clear();
+      while(true) {
+        var newNotices = await UolHomePage.GoScan();
+        foreach(var notice in newNotices) {
+          if(notice.PublishNotes.Length > 1) {
+            Console.WriteLine($"\t{notice.Title}\n\t\t{notice.Description}\n >> {notice.Link}\n");
+            Console.WriteLine($"  {notice.Autor} - {notice.AutorLocation} - {notice.PublishDate}");
+            Console.WriteLine($"\t{notice.PublishNotes}");
+            Console.WriteLine("====================================================================\n");
+            Thread.Sleep(1000); // espera 1 segundo
+            Console.Clear(); // limpa o console
+          }
         }
       }
     }
